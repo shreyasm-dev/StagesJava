3ffb237437819f1acc72583ca364604962ace7b7
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-05-10 11:43:56 -0500

    Add controller for about

---
+++ src/main/java/apcs/shreyas/stages/controllers/views/AboutController.java
@@ -0,0 +1,12 @@
+package apcs.shreyas.stages.controllers.views;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AboutController implements Initializable {
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+  }
+}
